1597098d9eb3460961b2ca89280ec236539b378e
JonathanVaughan <jonathan.vaughan@live.com>
2020-05-12 18:51:05 +0000

    up to stage 5 for tomorrow

--- application/__init__.py
+++ application/__init__.py
@@ -4,5 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']=getenv('FLASK_BLOG_DB_URI')
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 db = SQLAlchemy(app)
 from application import routes
